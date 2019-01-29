603ed16d0fcf47de913e56dc1c278aa8c4c766ea
SMB <sethmichaelbrown@gmail.com>
2019-01-29 14:02:46 -0700

    Working home screen

---
+++ src/components/Chat.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import {
+  View,
+  Text
+} from 'react-native'
+
+class Chat extends React.Component{
+  render(){
+    return (
+      <View>
+        <Text>
+          Hello Chat
+        </Text>
+      </View>
+    )
+
+  }
+}
+
+export default Chat;
